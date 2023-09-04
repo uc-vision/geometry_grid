Mostly fix point query gradient..!

geometry_grid/test/helper/grid.py
@@ -19,33 +19,39 @@ def test_with(f,
   n:int = 1000, 
   seg_length:float = 1.0,
 
-  n_points:int = 100000, 
-  radius:float=0.5):
+  radius:float=0.5,
+  noise_std=0.2):
 
-  segs = random_segments(bounds, length_range=(seg_length * 0.5, seg_length * 2.0), n=n)
+  segs = random_segments(bounds, 
+      length_range=(seg_length * 0.5, seg_length * 2.0), n=n)
+
 
   grid = ti_geom.Grid.fixed_size(bounds, grid_size)
   obj_grid = DynamicGrid.from_torch(grid, segs, max_occupied=64)
 
-  for i in range(10):
-    points = around_segments(segs, n_points, radius, point_std=0.1)
+  for i in range(1000):
+    n_points = int(torch.randint(10, 100, ()).item())
+
+    points = around_segments(segs, n_points, radius, point_std=noise_std)
     f(obj_grid, segs, points, radius)
 
 
 def test_grid_with(f, seed=0):
+  print(f"seed={seed}")
   torch.manual_seed(seed)
   grid_size = tuple(torch.randint(4, 32, (3,)).tolist())
 
   n = int(torch.randint(10, 100, ()).item())
   seg_length = (torch.rand(()) * 5.0 + 0.1).item()
 
-  n_points = int(torch.randint(1000, 10000, ()).item())
   radius = (torch.rand(()) * 1.0 + 0.1).item()
+  noise_std = torch.rand(() ).item() * radius
+
 
   bounds = torch_geom.AABox(torch.tensor([-10.0, -10.0, -10.0]), 
     torch.tensor([10.0, 10.0, 10.0]))
 
-  print(f"grid_size={grid_size}, n={n}, seg_length={seg_length}, n_points={n_points}, radius={radius}")
-  test_with(f, bounds, grid_size, n, seg_length, n_points, radius)
+  print(f"grid_size={grid_size}, n={n}, seg_length={seg_length}, radius={radius} noise_std={noise_std}")
+  test_with(f, bounds, grid_size, n, seg_length, radius)
 
 

geometry_grid/test/test_grid.py
@@ -16,11 +16,14 @@ def test_distances(grid, segs, points, radius):
     dist1, idx1 = tg.point_query.point_query(grid, points, max_distance=radius * 2)
     dist2, idx2 = min_distances(segs, points, max_distance=radius * 2)
 
+
     assert (idx1 == idx2).all()
     assert torch.allclose(dist1, dist2)
 
-    valid = idx1[idx1 >= 0].long()
-    dist3 = batch_distances(segs[valid], points[idx1 >= 0])
+    valid_points = idx1 >= 0
+    valid_segs = idx1[idx1 >= 0].long()
+
+    dist3 = batch_distances(segs[valid_segs], points[valid_points])
 
     assert torch.allclose(dist1[idx1 >= 0], dist3)
 
@@ -29,39 +32,32 @@ def test_distances(grid, segs, points, radius):
     assert torch.allclose(dist1, dist4)
     assert (idx4 == idx1).all()
 
-    
-    loss4 = dist4.sum()
+    loss4 = dist4[valid_points].sum()
     loss4.backward()
 
     grad4 = points.grad.clone()
     points.grad.zero_()
 
-    print(grad4)
-
-    assert torch.allclose(segs[idx4].to_vec(), grid.get_object_vecs(idx4))
-
-    dist5 = torch_func.distance.batch_distances(segs[idx1], points)
+    assert torch.allclose(segs[valid_segs].to_vec(), grid.get_object_vecs(valid_segs.to(torch.int32)))    
+    dist5 = torch_func.distance.batch_distances(segs[valid_segs], points[valid_points])
     
     loss5 = dist5.sum()
     loss5.backward()
 
     grad5 = points.grad.clone()
-    assert torch.allclose(dist1, dist5)
-
-    assert torch.allclose(grad4, grad5)
+    assert torch.allclose(dist1[valid_points], dist5)
+    assert torch.allclose(grad4, grad5, atol=1e-5)
 
 
 
     
 
 
-
-
 if __name__ == "__main__":
 
-  torch.set_printoptions(precision=3, sci_mode=False, linewidth=120)
+  torch.set_printoptions(precision=8, sci_mode=False, linewidth=120)
   ti.init(arch=ti.gpu, debug=False, offline_cache=True, log_level=ti.INFO)
 
   for i in range(10):
-    test_grid_with(test_distances, seed=i)
+    test_grid_with(test_distances, seed=i + 3)
 